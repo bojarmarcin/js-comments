095a973a3afab750149bc6d6d83cefe695f61bc1
Marcin Bojarski <boj.marcin@gmail.com>
2018-02-26 15:09:50 +0100

    Add comments to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// alert('My first alert');
+/*
+Lorem ipsum dolor sit amet ultricies accumsan.
+Quisque in dictum tempus, fringilla ligula turpis, et magna.
+In hac habitasse platea dictumst.
+Cum sociis natoque penatibus et netus et magnis
+dis parturient montes, nascetur ridiculus mus. Donec nunc.
+Suspendisse justo vulputate leo nec erat volutpat.
+Nunc viverra, enim sapien accumsan nunc.
+*/
+// Lorem ipsum dolor sit amet quam. Nulla venenatis nulla erat.
